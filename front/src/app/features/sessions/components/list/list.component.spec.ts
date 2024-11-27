a4749f1dcba25a4158fb6ea7cd4f57438fb2cabd
Tanouu <ethanpachjeco.t@gmail.com>
2024-11-27 13:47:23 +0100

    Tests sessions, list finalisés

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -2,10 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { expect } from '@jest/globals';
+import { of } from 'rxjs';
 import { SessionService } from 'src/app/services/session.service';
-
+import { SessionApiService } from '../../services/session-api.service';
 import { ListComponent } from './list.component';
+import { expect } from '@jest/globals';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -15,15 +17,24 @@ describe('ListComponent', () => {
     sessionInformation: {
       admin: true
     }
-  }
+  };
+
+  const mockSessionApiService = {
+    all: jest.fn().mockReturnValue(of([
+      { id: 1, name: 'Session 1', date: new Date(), users: [] },
+      { id: 2, name: 'Session 2', date: new Date(), users: [] }
+    ]))
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{ provide: SessionService, useValue: mockSessionService }]
-    })
-      .compileComponents();
+      imports: [HttpClientModule, MatCardModule, MatIconModule, RouterTestingModule],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
@@ -33,4 +44,15 @@ describe('ListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display a list of sessions', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    // Sélectionner uniquement les sessions avec la classe .item
+    const sessionElements = compiled.querySelectorAll('.item');
+    // Assertions
+    expect(sessionElements.length).toBe(2); // Nombre de sessions attendues
+    expect(sessionElements[0].textContent).toContain('Session 1');
+    expect(sessionElements[1].textContent).toContain('Session 2');
+  });
+
 });
