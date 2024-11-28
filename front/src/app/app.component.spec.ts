cb63af23fc9a823743addbfdb0afee0b67232c73
Tanouu <ethanpachjeco.t@gmail.com>
2024-11-28 16:24:52 +0100

    Test logout finalisés

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -5,9 +5,20 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
+import {SessionService} from "./services/session.service";
+import {Router} from "@angular/router";
+import {AuthService} from "./features/auth/services/auth.service";
 
 
 describe('AppComponent', () => {
+  let router: Router;
+  let sessionService: SessionService;
+
+  const mockSessionService = {
+    $isLogged: jest.fn(),
+    logOut: jest.fn()
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -18,12 +29,28 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        AuthService
+      ]
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call logOut and navigate to home on logout', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    app.logout();
+    expect(sessionService.logOut).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
   });
 });
